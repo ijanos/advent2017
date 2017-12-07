3965d7c45a7a3f353dd8d65ce82bbe32c63716cc
János Illés <ijanos@gmail.com>
2017-12-07 12:42:19 +0100

    cleaner code with namedtuple

--- python/day07/solve.py
+++ python/day07/solve.py
@@ -2,9 +2,11 @@
 
 import fileinput
 from itertools import chain
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
 programs = {}
+Program = namedtuple('Program', ['weight', "children"])
+
 for line in fileinput.input():
     line = line.strip().split(maxsplit=3)
     name, weight = line[0], int(line[1].strip('()'))
@@ -12,22 +14,23 @@ for line in fileinput.input():
         children = [child.strip() for child in line[3].split(',')]
     else:
         children = []
-    programs[name] = (weight, children)
+    programs[name] = Program(weight, children)
 
 names = programs.keys()
-all_children = chain(*[children for _, children in programs.values()])
+all_children = chain(*[p.children for p in programs.values()])
 
 part1 = (set(names) - set(all_children)).pop()
 print("Part 1:", part1)
 
-def calc_weight(n):
-    return programs[n][0] + sum(calc_weight(child) for child in programs[n][1])
+def calc_weight(name):
+    return programs[name].weight + sum(calc_weight(child) for child in programs[name].children)
 
 cur = part1
 diff = 0
+
 while True:
     bins = defaultdict(list)
-    for child in programs[cur][1]:
+    for child in programs[cur].children:
         bins[calc_weight(child)].append(child)
     if len(bins) > 1:
         (w0, wrong), (w1, _) = sorted(bins.items(), key=lambda i: len(i[1]))
@@ -36,4 +39,4 @@ while True:
     else:
         break
 
-print("Part 2:", programs[cur][0] + diff)
+print("Part 2:", programs[cur].weight + diff)
