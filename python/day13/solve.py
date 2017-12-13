31ebbc197636708eede01b6f9a755f0a1c5a0149
János Illés <ijanos@gmail.com>
2017-12-13 17:29:45 +0100

    a little faster version of day13 python solution

--- python/day13/solve.py
+++ python/day13/solve.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
 
 import fileinput
-from collections import deque
-from copy import deepcopy
 
 
-firewall = {}
-last = 0
+firewall = []
 
 for line in fileinput.input():
     depth, runrange = (int(n.strip(':')) for n in line.split())
-    firewall[depth] = 2 * runrange - 2
-    last = max(depth, last)
+    runrange = 2 * runrange - 2
+    firewall.append((depth, runrange))
 
 def run(delay, fast=False):
     severity = 0
     caught = False
-    for i in range(last+1):
-        if i in firewall and (i + delay) % firewall[i] == 0:
-            severity += i * ((firewall[i] + 2) // 2)
+    for i, runrange in firewall:
+        if (i + delay) % runrange == 0:
             caught = True
             if fast:
                 break
+            severity += i * ((runrange + 2) // 2)
     return (caught, severity)
 
-(_, part1) = run(0)
+caught, part1 = run(0)
 print("Part 1:", part1)
 
 wait = 0
-while True:
-    (caught, _) = run(wait, fast=True)
-    if not caught:
-        break
+while caught:
+    caught, _ = run(wait, fast=True)
     wait += 1
 
 print("Part 2:", wait)
